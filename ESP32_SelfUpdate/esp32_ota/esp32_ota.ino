c7e2f7787bb1b79303c5c4f0dc3b28ea47e2da93
jfnunez-dev <73258355+jfnunez-dev@users.noreply.github.com>
2021-10-18 11:38:31 -0300

    Update esp32_ota.ino

--- ESP32_SelfUpdate/esp32_ota/esp32_ota.ino
+++ ESP32_SelfUpdate/esp32_ota/esp32_ota.ino
@@ -11,8 +11,8 @@ const char * password = "helloworld";
 String FirmwareVer = {
   "2.2.1"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/jfnunez-dev/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/jfnunez-dev/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/jfnunez-dev/newrepo/master/esp32_ota/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/jfnunez-dev/newrepo/master/esp32_ota/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
